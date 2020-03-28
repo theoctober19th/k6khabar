201e6a585cf5f52f79ab811f6ca37a06ca264e59
Bikalpa Dhakal <theoctober19th@gmail.com>
2020-03-28 14:27:50 +0545

    register page updated

--- userapp/urls.py
+++ userapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.contrib import admin
 
-from .views import loginview
+from .views import loginview, registerview
 
 urlpatterns = [
-    path('login/', loginview, name='login')
+    path('login/', loginview, name='login'),
+    path('register/', registerview, name="register")
 ]
--- userapp/views.py
+++ userapp/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from .models import UserModel
 
 
 def loginview(request):
     return render(request, 'userapp/login.html')
+
+
+def registerview(request):
+    if request.method == 'POST':
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        status = request.POST.get('status')
+        profile_pic = request.POST.get('profilepic')
+
+        user = User(username=username, password=password, email=email,
+                    first_name=fname, last_name=lname, is_active=True)
+        user.save()
+        usermodel = UserModel(auth=user, status=status,
+                              profile_picture=profile_pic)
+        usermodel.save()
+        return redirect('index')
+    else:
+        return render(request, 'userapp/register.html')
